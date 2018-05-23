da29b743369726c778c06faff183ad3fd8ac308d
Kai Germaschewski <kai.germaschewski@unh.edu>
2018-05-23 10:48:29 -0400

    cpaw: higher resolution test, 2d test

--- mhd/tests/t008a/t008a.sh
+++ mhd/tests/t008a/t008a.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 
 mpirun -n 1 ../mhd_cpaw \
-    --mrc_domain_mx 128 --mrc_domain_my 1 --mrc_domain_mz 1 \
+    --mrc_domain_mx 128 --mrc_domain_my 128 --mrc_domain_mz 1 \
     --mrc_domain_npx 1 --mrc_domain_npy 1 --mrc_domain_npz 1 \
     \
     --ggcm_mhd_d_i 0. \
@@ -12,7 +12,7 @@ mpirun -n 1 ../mhd_cpaw \
     --ggcm_mhd_step_riemann rusanov \
     --ggcm_mhd_step_time_integrator euler \
     --ggcm_mhd_step_do_nwst \
-    --ggcm_mhd_thx .8 \
+    --ggcm_mhd_thx .4 \
     \
     --mrc_ts_output_every_time .1 \
     --ggcm_mhd_diag_fields rr1:uu1:rv1:b1:j:divb:rr:pp:v:b \
